880905b973925e6dc42b8fea8c7523b5c870ef37
trejo09g <104469081+trejo09g@users.noreply.github.com>
2023-10-04 19:58:14 -0600

    Add files via upload

---
+++ EVA2_1_MULTITAREA/src/main/java/EVA2_1_MULTITAREA.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author garci
+ */
+public class EVA2_1_MULTITAREA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

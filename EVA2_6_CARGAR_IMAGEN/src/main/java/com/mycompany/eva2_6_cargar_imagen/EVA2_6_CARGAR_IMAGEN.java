4093a2ed10911cce9264fb42f53b5e877cf1745a
trejo09g <104469081+trejo09g@users.noreply.github.com>
2023-10-11 18:44:48 -0600

    Add files via upload

---
+++ EVA2_6_CARGAR_IMAGEN/src/main/java/com/mycompany/eva2_6_cargar_imagen/EVA2_6_CARGAR_IMAGEN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_6_cargar_imagen;
+
+/**
+ *
+ * @author garci
+ */
+public class EVA2_6_CARGAR_IMAGEN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

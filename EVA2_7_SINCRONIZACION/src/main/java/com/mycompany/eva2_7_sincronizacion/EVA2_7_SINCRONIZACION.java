18ab0c5b219ef9860434f78ec629ae2ff6258e90
trejo09g <104469081+trejo09g@users.noreply.github.com>
2023-10-17 18:50:29 -0600

    Add files via upload

---
+++ EVA2_7_SINCRONIZACION/src/main/java/com/mycompany/eva2_7_sincronizacion/EVA2_7_SINCRONIZACION.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_7_sincronizacion;
+
+/**
+ *
+ * @author garci
+ */
+public class EVA2_7_SINCRONIZACION {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

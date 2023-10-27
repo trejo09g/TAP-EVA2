2556ce89fd73d0a94cc4f85c7513d84b6156ab34
trejo09g <104469081+trejo09g@users.noreply.github.com>
2023-10-26 18:36:37 -0600

    Add files via upload

---
+++ EVA2_9_REUSAR_COMPONENTES/src/main/java/com/mycompany/eva2_9_reusar_componentes/EVA2_9_REUSAR_COMPONENTES.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_9_reusar_componentes;
+
+/**
+ *
+ * @author garci
+ */
+public class EVA2_9_REUSAR_COMPONENTES {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
